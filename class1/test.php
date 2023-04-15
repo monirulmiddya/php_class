006a37ea689c81111735f7f878c801368862a248
Monirul Middya <=>
2023-04-16 00:29:16 +0530

    first class

---
+++ class1/test.php
@@ -0,0 +1,41 @@
+<?php 
+
+
+// echo "Monirul Middya";
+// echo "Monirul Middya";
+// echo "Monirul Middya";
+
+// echo "Monirul Middya";
+
+// $text = "Middya";
+// $text123 = "Middya";
+
+// echo "bajrul {$text} <br>";
+// echo "Najibul $text <br>";
+// echo "Fijauddin ".$text." <br>";
+// echo "Musta $text <br>";
+
+// $x = 5; // global scope
+ 
+// function save() {
+//   // using x inside this function will generate an error
+//   echo "<p>Variable x inside function is: $x</p>";
+// } 
+
+// function calcel() {
+//     // using x inside this function will generate an error
+//     echo "<p>Variable x inside function is: $x</p>";
+// } 
+
+// save();
+
+// echo "<p>Variable x outside function is: $x</p>";
+
+
+echo "<h2>PHP is Fun!</h2> hgdkghdk";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+
+?>
