c5eec2d4a68394ede88f54fef737db91ee90247b
Monirul Middya <=>
2023-04-17 00:08:18 +0530

    class 2

---
+++ class2/test.php
@@ -0,0 +1,63 @@
+<?php
+
+// String
+
+$string = 'This is a string';
+
+// echo $string;
+
+// echo gettype($string);
+
+
+// Integer
+
+$Integer = 12345;
+
+// echo gettype($Integer);
+
+// Float (floating point numbers - also called double)
+
+$Float = 12345.12;
+
+// echo gettype($Float);
+
+
+// Boolean
+
+$Boolean = false; // bool
+
+// echo gettype($Boolean);
+
+// Array
+
+// $Array = [9,5,7,4,8,7];
+$Array = array("Naji","Baju","fija","musta",5478,true);
+
+// echo gettype($Array);
+
+// echo "<pre>";
+// print list of data
+// print_r($Array);
+// echo $Array[2];
+
+// print data with datatype
+// echo var_dump($Array);
+
+// Object
+
+$object = (object)["std1"=> "bajrul","std2"=>"musta"];
+
+// echo "<pre>";
+// print_r($object);
+// print_r($object->std1);
+
+
+// NULL
+
+$null = null;
+
+// echo $null;
+// echo gettype($null);
+// var_dump($null);
+
+?>
