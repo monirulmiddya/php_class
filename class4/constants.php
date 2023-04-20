d3651e1d886f3d80e07462862e52dc1d233ac89f
Monirul Middya <=>
2023-04-21 00:10:10 +0530

    class 4

---
+++ class4/constants.php
@@ -0,0 +1,46 @@
+<?php
+
+// To create a constant, use the define() function.
+
+// define(name, value, case-insensitive)
+
+// define("APP_NAME", "WEB APP");
+
+// echo APP_NAME;
+
+
+
+// define("cars", ["Alfa Romeo","BMW","Toyota"]);
+// echo cars[0];
+
+
+
+// define("GREETING", "Saved success");
+
+// function save() {
+//   echo GREETING;
+// }
+ 
+// save();
+
+
+// $text = "Musta";
+
+// echo $text;
+
+// echo "<br>";
+
+// $text = "Mota";
+
+
+// echo $text;
+
+
+// define("text", "Musta");
+// echo text;
+// echo "<br>";
+// define("text", "Mota");
+// echo text;
+
+
+?>
---
+++ class4/math.php
@@ -0,0 +1,43 @@
+<?php
+
+// The pi() function returns the value of PI:
+
+// echo(pi());
+
+
+
+// PHP min() and max() Functions
+// The min() and max() functions can be used to find the lowest or highest value in a list of arguments:
+
+// echo min(0, 150, 30, 20, -8, -200);
+// echo "<br>";
+// echo(max(0, 150, 30, 20, -8, -200));
+
+// echo max(-4545,0);
+
+$earning = 10000;
+$deduct = 12000;
+
+$salary = $earning - $deduct;
+
+
+// echo max($salary,0);
+
+// The abs() function returns the absolute (positive) value of a number:
+
+// echo abs($salary);
+
+
+// The sqrt() function returns the square root of a number:
+
+// echo(sqrt(64)); 
+
+// echo(round(1.50));
+// echo(round(1.49));
+
+// The rand() function generates a random number:
+
+// echo(rand(111111,999999));
+
+
+?>
